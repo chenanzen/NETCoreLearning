76935a33dd2dc9fe9a622bfe423ae4bacf0c8140
Ali SUSANTO <ali_susanto@nec.com.sg>
2023-03-20 16:25:46 +0800

    added blazor

--- ImageConsoleApp/ImageLibrary/DependencyInjection/ServiceCollectionExtensions.cs
+++ ImageConsoleApp/ImageLibrary/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,11 +1,6 @@
 ﻿using ImageLibrary.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ImageLibrary.DependencyInjection
 {
